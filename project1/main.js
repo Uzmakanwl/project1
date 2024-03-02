7f9f5461c8df1ef4ec5df9149ef0ae4381e3e07e
Uzmakanwl <154093505+Uzmakanwl@users.noreply.github.com>
2024-03-02 06:54:43 -0800

    Add files via upload

---
+++ project1/main.js
@@ -0,0 +1,9 @@
+// STRING
+var myString = "muzamil";
+console.log(myString);
+// NUMBER
+var box = 90;
+console.log(box);
+// BOOLEAN
+var myBool = false;
+console.log(myBool);
---
+++ project1/main.ts
@@ -0,0 +1,17 @@
+// STRING
+
+let myString: string = "muzamil";
+console.log(myString);
+
+
+// NUMBER
+
+
+let box: number = 90;
+console.log(box);
+
+
+// BOOLEAN
+
+const myBool: boolean = false;
+console.log(myBool);
